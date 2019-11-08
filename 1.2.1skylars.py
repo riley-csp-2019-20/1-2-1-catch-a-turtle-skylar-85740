a9adb38d7cfb3c4448a1da797437af9d196414b8
skylar-85740 <55498203+skylar-85740@users.noreply.github.com>
2019-11-08 10:35:02 -0500

    Update 1.2.1skylars.py

--- 1.2.1skylars.py
+++ 1.2.1skylars.py
@@ -1,30 +1,41 @@
 # a121_catch_a_turtle.py
 #-----import statements-----
 import turtle as trtl 
-fruit = trtl.Turtle()
+import random as rand
+
 
 
 #-----game configuration----
+fruit = trtl.Turtle()
 fruit.shape("circle")
 fruit.shapesize(4)
 fruit.fillcolor("red")
+fruit.speed(4)
 
 #-----initialize turtle-----
+
+
+
+#-----game functions--------
 def fruit_clicked(x, y):
     change_position()    
 
-#-----game functions--------
+def change_position():
+    fruit.penup()
+    fruit.hideturtle()
+    new_xcor = rand.randint(-400, 400)
+    new_ycor = rand.randint(-300, 300)
+    fruit.goto(new_xcor, new_ycor)
+    fruit.pendown()
+    fruit.showturtle()
+
+
 
 
 #-----events----------------
 fruit.onclick(fruit_clicked)
-import random as rand
-fruit_x = fruit.xcor()
-fruit_y = fruit.ycor()
-def change_position():
-    rand.randint(x,y)
-    new_xpos()
-    new_ypos()
+
+    
 
 
 
